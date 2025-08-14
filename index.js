91d5cd01a850e04c011922704a3aa0959b38cda0
sanjayimpact <sanjay.impactmindz@gmail.com>
2025-08-14 13:14:25 +0530

    update

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Hello CI/CD ✅"));
 app.get("/health", (req, res) => res.json({ ok: true }));
+app.get("/new",(req,res)=>{
+    res.send("New route");
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server listening on " + PORT));
